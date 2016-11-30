06457c9d65ca648400bc9cd477c023461b277594
vplanMan <1471782566@qq.com>
2016-11-30 11:20:33 +0800

    增加了——添加数据库的方法（词书表数据，词库表数据，例句表数据，单词学习表数据）

--- app/src/main/java/recitewords/apj/com/recitewords/db/dao/LexiconDao.java
+++ app/src/main/java/recitewords/apj/com/recitewords/db/dao/LexiconDao.java
@@ -1,6 +1,7 @@
 package recitewords.apj.com.recitewords.db.dao;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 
 import recitewords.apj.com.recitewords.activity.MainActivity;
 import recitewords.apj.com.recitewords.db.ReciteWordsSQLiteOpenHelper;
@@ -12,8 +13,21 @@ import recitewords.apj.com.recitewords.db.ReciteWordsSQLiteOpenHelper;
 public class LexiconDao {
     public ReciteWordsSQLiteOpenHelper helper;
 
+    String add_sql ="insert into lexicon(book_name,book_desc,word_num,word_num_study,word_num_grasp,word_num_review,userID) values " +
+            "(?,?,?,?,?,?,?)";
+
     public LexiconDao(Context context) {
         helper = new ReciteWordsSQLiteOpenHelper(context, MainActivity.dbName, 1);
+    }
+
+    /*
+    * 增加词库表方法
+    * */
+    public void addWord(String book_name,String book_desc,int word_num, int word_num_study,
+                        int word_num_grasp, int word_num_review,int userID) {
+        SQLiteDatabase db = helper.getWritableDatabase();
+        db.execSQL(add_sql, new Object[]{book_name,book_desc,word_num,word_num_study,word_num_grasp,word_num_review,userID});
+        db.close();
     }
 
 }
