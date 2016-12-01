9602903a4806c303e3f64b8d969a88d15bb08bff
cgt <782119927@qq.com>
2016-12-01 10:26:37 +0800

    修复显示ancd选项在不同分别率显示问题

--- app/src/main/java/recitewords/apj/com/recitewords/activity/LearnActivity.java
+++ app/src/main/java/recitewords/apj/com/recitewords/activity/LearnActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewTreeObserver;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
@@ -11,7 +12,7 @@ import android.widget.TextView;
 
 import recitewords.apj.com.recitewords.R;
 
-public class LearnActivity extends BaseActivity implements View.OnClickListener {
+public class LearnActivity extends BaseActivity implements View.OnClickListener, ViewTreeObserver.OnGlobalLayoutListener {
 
     public class ViewHolder {
         public LinearLayout ll_choice;  //选择题模式--中间按钮--不认识
@@ -28,7 +29,7 @@ public class LearnActivity extends BaseActivity implements View.OnClickListener 
         public TextView tv_memory_cognize;  //回忆模式--认识
         public TextView tv_memory_incognizance;  //回忆模式--不认识
         public RelativeLayout rl_learn;   //学习的根布局
-        public LinearLayout ll_example;  //例句的根布局
+        public FrameLayout fl_example;  //例句的根布局
         public LinearLayout ll_show_word;  //顶部显示的单词，音标和学习情况根布局
     }
 
@@ -46,7 +47,7 @@ public class LearnActivity extends BaseActivity implements View.OnClickListener 
     private void initView() {
         holder = new ViewHolder();
         holder.rl_learn = findViewByIds(R.id.rl_learn);
-        holder.ll_example = findViewByIds(R.id.ll_example);
+        holder.fl_example = findViewByIds(R.id.fl_example);
         holder.ll_choice = findViewByIds(R.id.ll_choice);
         holder.ll_incognizance=findViewByIds(R.id.ll_incognizance);
         holder.ll_memory=findViewByIds(R.id.ll_memory);
@@ -56,14 +57,6 @@ public class LearnActivity extends BaseActivity implements View.OnClickListener 
         holder.tv_word_information=findViewByIds(R.id.tv_word_information);
         holder.ll_show_word=findViewByIds(R.id.ll_show_word);
 
-        holder.ll_show_word.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-            @Override
-            public void onGlobalLayout() {
-                int height = holder.ll_show_word.getHeight();  //和例句一样的高度
-                holder.ll_example.setMinimumHeight(height);
-                holder.ll_show_word.getViewTreeObserver().removeOnGlobalLayoutListener(this);
-            }
-        });
     }
 
     private void initData() {
@@ -73,14 +66,17 @@ public class LearnActivity extends BaseActivity implements View.OnClickListener 
         int[] images = intent.getIntArrayExtra("images");
         holder.rl_learn.setBackgroundResource(images[backgroundNum]);
         holder.rl_learn.getBackground().setAlpha(70);  //更改学习界面透明度
-        holder.ll_example.getBackground().setAlpha(70);  //更改例句界面透明度
-
+        holder.fl_example.getBackground().setAlpha(70);  //更改例句界面透明度
     }
 
     private void initEvent() {
+        //选择题模式下点击不认识
         holder.ll_choice.setOnClickListener(this);
+        //监听视图树获取高度
+        holder.ll_show_word.getViewTreeObserver().addOnGlobalLayoutListener(this);
     }
 
+    //点击事件回调方法
     @Override
     public void onClick(View v) {
         switch (v.getId()){
@@ -97,6 +93,14 @@ public class LearnActivity extends BaseActivity implements View.OnClickListener 
             default:
                 break;
         }
+    }
+
+    //视图树的回调方法
+    @Override
+    public void onGlobalLayout() {
+        int height = holder.ll_show_word.getHeight();  //和例句一样的高度
+        holder.fl_example.setMinimumHeight(height);  //设置例句高度
+        holder.ll_show_word.getViewTreeObserver().removeOnGlobalLayoutListener(this);
     }
 
 
