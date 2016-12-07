1feffef783b9d679ceec0f5a3b918fb58653b8b2
chenguitang <782119927.com>
2016-12-07 09:23:44 +0800

    增加复习的拼写测试界面

---
+++ app/src/main/java/recitewords/apj/com/recitewords/activity/ReviewSpellActivity.java
@@ -0,0 +1,15 @@
+package recitewords.apj.com.recitewords.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import recitewords.apj.com.recitewords.R;
+
+public class ReviewSpellActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review_spell);
+    }
+}
